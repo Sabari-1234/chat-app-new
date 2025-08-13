a69552a69e64e1ee66cc8e3147c04800c77cda27
sabarinathan.s <sabarinathan.s@zohocorp.com>
2025-08-13 12:20:22 +0530

    leftpanelprovider place changed

--- app/chatPanel/page.tsx
+++ app/chatPanel/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 import ChatPanel from "@/pages/ChatPanel";
-import { LeftPanelProvider } from "@/contexts/LeftPanelContext";
-
 const page = () => {
-  return (
-    <LeftPanelProvider>
-      <ChatPanel />
-    </LeftPanelProvider>
-  );
+  return <ChatPanel />;
 };
 
 export default page;
